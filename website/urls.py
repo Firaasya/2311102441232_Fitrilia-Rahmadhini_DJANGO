9b844204a967c0129d286dd68e91d8114985fa0b
Fitrilia <firasoohyuck@gmail.com>
2025-03-15 20:36:47 +0800

    perbaikan konfigurasi

--- website/urls.py
+++ website/urls.py
@@ -1,29 +1,11 @@
-"""
-URL configuration for website project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-from . import views  # Sesuaikan dengan aplikasi yang sedang dibuat
+from django.contrib import admin
+from django.urls import include, path
+from guetbook import views  # Pastikan ini sudah benar
+from guetbook.views import home
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Sesuaikan dengan view yang ada
+    path('', views.home, name='home'),  # Menggunakan home dari guestbook/views.py
+    path('guetbook/', include('guetbook.urls')),
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
-
-# Tambahkan baris ini untuk melayani file statis di mode development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-
--- website/views.py
+++ website/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def home(request):
-    return render(request, 'index.html')  
+    return render(request, 'about.html')  
+
+def home(request):
+    return HttpResponse("Welcome to the Homepage!")
